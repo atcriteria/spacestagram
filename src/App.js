32f4ecb19c79865f3a6b2bc59340a4c780576e45
dude lao <brycelipscomb@gmail.com>
2021-09-16 16:44:26 -0400

    hooked up the axios request to the api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [state, setState] = useState(null)
+  const key = process.env.REACT_APP_API_KEY
+  const date = new Date()
+  const year = date.getFullYear()
+  const month = (date.getMonth() + 1).toPrecision(2)
+  const day = date.getDay() +1
+  console.log(`Today's date: ${year}/${month}/${day}`)
+
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod`, {
+      params: {
+        count: 10,
+        api_key: key
+      }
+    })
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
   return (
     <div className="App">
-
+      <h1>App</h1>
     </div>
   );
 }
