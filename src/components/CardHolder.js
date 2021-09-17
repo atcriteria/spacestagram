4a5e8bd0d8439f792f5c13401fd43e35b5633f43
dude lao <brycelipscomb@gmail.com>
2021-09-17 18:45:39 -0400

    grabbing local data in the CardHolder component so we only have to parse it a single time rather than each time we render an image card

--- src/components/CardHolder.js
+++ src/components/CardHolder.js
@@ -4,12 +4,15 @@ import ImageCard from './ImageCard';
 
 export default function CardHolder(props){
     const {data} = props;
+    const localData = JSON.parse(localStorage.getItem("data"))
+
     return(
         <section className="cardHolder">
             {
                 (!data)  ? "Loading images...." :
                 data.map((imageData) => {
-                    return <ImageCard imageData={imageData} key={imageData.date}/>
+                    let isLiked = (localData && localData[imageData.date]) ? true : false;
+                    return <ImageCard imageData={imageData} key={imageData.date} isLiked={isLiked} />
                 })
             }
         </section>
